98de9ae3cb568afd4b51225cd92d4acaff1a7e08
Kyy <68399818+snkykun@users.noreply.github.com>
2021-04-18 17:54:54 -0400

    update

---
+++ lib/bot/__init__.py
@@ -0,0 +1,45 @@
+from apscheduler.schedulers.asyncio import AsyncIOScheduler
+from discord.ext.commands import Bot as BotBase
+
+PREFIX = "$"
+OWNER_IDS = [159824032710000640]
+
+
+class Bot(BotBase):
+    def __init__(self):
+        self.PREFIX = PREFIX
+        self.ready = False
+        self.guild = None
+        self.scheduler = AsyncIOScheduler()
+
+        super().__init__(command_prefix=PREFIX, owner_ids=OWNER_IDS)
+
+    def run(self, version):
+        self.VERSION = version
+
+        with open("solid-palm-tree\lib\bot\lib\bot\token.txt", "r", encoding="utf-8") as tf:
+            self.TOKEN = tf.read()
+
+        print("running bot...")
+        super().run(self.TOKEN, reconnect=True)
+
+    async def on_connect(self):
+        print("bot connected")
+
+    async def on_disconnect(self):
+        print("bot disconnected")
+
+    async def on_ready(self):
+        if not self.ready:
+            self.ready = True
+            self.guild = self.get_guild(743941628020129853)
+            print("bot ready")
+
+        else:
+            print("bot reconnected")
+
+    async def on_message(self, message):
+        pass
+
+
+bot = Bot()
